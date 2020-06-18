3c1fac7eb428eb61e39562fb3eb648569cc43a58
shubhamkumar1689 <60147097+shubhamkumar1689@users.noreply.github.com>
2020-06-18 12:28:19 +0530

    sev

--- release.groovy
+++ release.groovy
@@ -13,6 +13,18 @@ buildPipelineView('myild') {
 }
 
 
+job('example') {
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('example/project-a')
+            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
 
 job("mygit"){
   scm{
